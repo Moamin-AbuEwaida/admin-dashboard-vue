6e3e2c64c6eac30e8f64f5187a589ed2c783df7c
Moamin <moamin.salamah@gmail.com>
2023-02-24 09:31:16 +0100

    repo set up

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app id="inspire">
+    <v-main style="background: #f5f5f540">
+      <v-container class="py-8 px-6" fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+  data: () => ({
+    cards: ["Today", "Yesterday"],
+    drawer: null,
+  }),
+  methods: {},
+};
+</script>
